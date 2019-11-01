6d53bb612c130fd02f9ca05524905337cd9deec1
Liem Vo <liemvt@gmail.com>
2019-11-01 22:15:43 +1300

    Day 18 of #100DaysOfSwiftUI

--- WeSplit/WeSplit/ContentView.swift
+++ WeSplit/WeSplit/ContentView.swift
@@ -10,21 +10,24 @@ import SwiftUI
 
 struct ContentView: View {
 	@State private var checkAmout = ""
-	@State private var numberOfPeople = 2
+	@State private var numberOfPeople = ""
 	@State private var tipPercentage = 2
 	
 	let tipPercentages = [10, 15, 20, 25, 0]
 	
-	var totalPerPerson: Double {
-		let peopleCount = Double(numberOfPeople + 2)
+	var total: Double {
 		let tipSelection = Double(tipPercentages[tipPercentage])
 		let orderAmount = Double(checkAmout) ?? 0
 		
 		let tipValue = orderAmount / 100 * tipSelection
 		let grandTotal = orderAmount + tipValue
 		
-		let amountPerPerson = grandTotal / peopleCount
-		
+		return grandTotal
+	}
+	
+	var totalPerPerson: Double {
+		let peopleCount = Double(numberOfPeople) ?? 2
+		let amountPerPerson = total  / peopleCount
 		return amountPerPerson
 	}
 	
@@ -34,11 +37,9 @@ struct ContentView: View {
 				Section {
 					TextField("Amount", text: $checkAmout)
 						.keyboardType(.decimalPad)
-					Picker("Number of peope", selection: $numberOfPeople) {
-						ForEach(2 ..< 100) {
-							Text("\($0) people")
-						}
-					}
+				
+					TextField("Number of people", text: $numberOfPeople)
+						.keyboardType(.decimalPad)
 				}
 				
 				Section (header: Text("How much tip do you want to leave?")) {
@@ -49,11 +50,15 @@ struct ContentView: View {
 					}.pickerStyle(SegmentedPickerStyle())
 				}
 				
-				Section {
+				Section (header: Text("Amount per person")){
 					Text("$\(totalPerPerson, specifier: "%.2f")")
 				}
+				
+				Section (header: Text("Total amount")){
+					Text("$\(total, specifier: "%.2f")")
+				}
 			}
-		.navigationBarTitle("WeSplit")
+			.navigationBarTitle("WeSplit")
 		}
 	}
 }
