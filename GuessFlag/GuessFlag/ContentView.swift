fcb42274413fb6fc2ca76f32ee8282d03df60e14
Liem Vo <liemvt@gmail.com>
2019-11-02 08:24:31 +1300

    Day 20 - 21 - 22 of #100DaysOfSwiftUI

---
+++ GuessFlag/GuessFlag/ContentView.swift
@@ -0,0 +1,74 @@
+//
+//  ContentView.swift
+//  GuessFlag
+//
+//  Created by Liem Vo on 11/2/19.
+//  Copyright © 2019 Liem Vo. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+	@State private var countries = ["estonia", "france", "germany", "ireland", "italy", "monaco", "nigeria", "poland", "russia", "spain", "uk", "us"].shuffled()
+	@State private var correctAnswer = Int.random(in: 0...2)
+	
+	@State private var showingScore = false
+	@State private var scoreTitle = ""
+	
+	@State private var score = 0
+	
+	var body: some View {
+		ZStack {
+			LinearGradient(gradient: Gradient(colors: [.blue, .black]), startPoint: .top, endPoint: .bottom).edgesIgnoringSafeArea(.all)
+			VStack (spacing: 30) {
+				VStack {
+					Text("Tap thge flag of")
+						.foregroundColor(.white)
+					
+					Text(countries[correctAnswer].capitalized)
+						.foregroundColor(.white)
+						.font(.largeTitle)
+						.fontWeight(.black)
+				}
+				
+				ForEach(0 ..< 3) { number in
+					Button(action: {
+						self.flagTapped(number)
+					}) {
+						Image(self.countries[number])
+							.renderingMode(.original)
+							.clipShape(Capsule())
+							.overlay(Capsule().stroke(Color.black, lineWidth: 1)).shadow(color: .black, radius: 2)
+					}
+				}
+				
+				Text("Score: \(score)")
+					.foregroundColor(.white)
+				
+				Spacer()
+			}
+		}
+		.alert(isPresented: $showingScore) {
+			Alert(title: Text(scoreTitle), message: Text("Your score is \(score)"), dismissButton: .default(Text("Continue")) {
+				self.askQuestion()
+				})
+		}
+	}
+	
+	private func flagTapped(_ number: Int) {
+		scoreTitle = number == correctAnswer ? "Correct" : "Wrong! That flag is \(countries[correctAnswer])"
+		score = number == correctAnswer ? score + 1 : score - 1
+		showingScore = true
+	}
+	
+	private func askQuestion() {
+		countries.shuffled()
+		correctAnswer = Int.random(in: 0...2)
+	}
+}
+
+struct ContentView_Previews: PreviewProvider {
+	static var previews: some View {
+		ContentView()
+	}
+}
--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # 100daysOfSwiftUI
 
-1. [WeSplit](WeSplit)
+1. [Project 1 WeSplit](WeSplit)
+2. [Challenge UnitConvert](UnitConvert)
+3. [Project 3 GuessFlag](GuessFlag)
