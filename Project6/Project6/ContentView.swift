daee2f2d88774d6aa79f74154a6621cd8f0d51c3
Liem Vo <liemvt@gmail.com>
2019-11-08 06:09:10 +1300

    Day 33 Animating gestures #100DaysOfSwiftUI

--- Project6/Project6/ContentView.swift
+++ Project6/Project6/ContentView.swift
@@ -9,20 +9,31 @@
 import SwiftUI
 
 struct ContentView: View {
-	@State private var animationAmount  = 0.0
-	var body: some View {
-		
-		Button("Tap Me") {
-			withAnimation(.interpolatingSpring(stiffness: 5, damping: 1)) {
-				self.animationAmount += 360
-			}
-		}
-		.padding(50)
-		.background(Color.red)
-		.foregroundColor(.white)
-		.clipShape(Circle())
-		.rotation3DEffect(.degrees(animationAmount), axis: (x: 0, y: 1, z: 0))
-	}
+	let letters = Array("Hello SwiftUI")
+    @State private var enabled = false
+    @State private var dragAmount = CGSize.zero
+
+    var body: some View {
+        HStack(spacing: 0) {
+            ForEach(0..<letters.count) { num in
+                Text(String(self.letters[num]))
+                    .padding(4)
+                    .font(.title)
+					.background(self.enabled ? Color.orange : Color.blue)
+					.foregroundColor(.white)
+                    .offset(self.dragAmount)
+                    .animation(Animation.default.delay(Double(num) / 20))
+            }
+        }
+        .gesture(
+            DragGesture()
+                .onChanged { self.dragAmount = $0.translation }
+                .onEnded { _ in
+                    self.dragAmount = .zero
+                    self.enabled.toggle()
+                }
+        )
+    }
 }
 
 struct ContentView_Previews: PreviewProvider {
