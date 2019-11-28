1acd9277452feaeae527cd672e3c7755aaa86d0d
Liem Vo <liemvt@gmail.com>
2019-11-28 18:21:43 +1300

    Day 66 of #100DaysOfSwiftUI

--- Instafilter/Instafilter/ContentView.swift
+++ Instafilter/Instafilter/ContentView.swift
@@ -10,25 +10,18 @@ import CoreImage
 import CoreImage.CIFilterBuiltins
 import SwiftUI
 
-class ImageSaver: NSObject {
-	func writeToPhotoAlbum(image: UIImage) {
-		UIImageWriteToSavedPhotosAlbum(image, self, #selector(saveError), nil)
-	}
-	
-	@objc private func saveError(_ image: UIImage, didFinishSavingWithError error: Error?, contextInfo: UnsafeRawPointer) {
-		print("Save finish")
-	}
-}
-
 struct ContentView: View {
 	
 	@State private var image: Image?
 	@State private var filterIntensity = 0.5
 	
 	@State private var showingImagePicker = false
-	@State private var inputImage: UIImage?
+	@State private var showingFilterSheet = false
 	
-	@State var currentFilter = CIFilter.sepiaTone()
+	@State private var inputImage: UIImage?
+	@State private var processedImage: UIImage?
+	
+	@State var currentFilter: CIFilter = CIFilter.sepiaTone()
 	let context = CIContext()
 	
 	
@@ -68,11 +61,19 @@ struct ContentView: View {
 				
 				HStack {
 					Button("Change Filter") {
-						
+						self.showingFilterSheet = true
 					}
 					Spacer()
 					Button("Save") {
-						
+						guard let processedImage = self.processedImage else { return }
+						let imageSaver = ImageSaver()
+						imageSaver.successHandler = {
+							print("Success!!!")
+						}
+						imageSaver.errorHandler = {
+							print("\($0.localizedDescription)")
+						}
+						imageSaver.writeToPhotoAlbum(image: processedImage)
 					}
 				}
 			}
@@ -80,6 +81,32 @@ struct ContentView: View {
 			.navigationBarTitle("Intafilter")
 			.sheet(isPresented: $showingImagePicker, onDismiss: loadImage) {
 				ImagePicker(image: self.$inputImage)
+			}
+			.actionSheet(isPresented: $showingFilterSheet){
+				ActionSheet(title: Text("Select a filter"), buttons: [
+					.default(Text("Crystallize")){
+						self.setFilter(CIFilter.crystallize())
+					},
+					.default(Text("Edges")) {
+						self.setFilter(CIFilter.edges())
+					},
+					.default(Text("Gaussian Blur")) {
+						self.setFilter(CIFilter.gaussianBlur())
+					},
+					.default(Text("Pixellate")) {
+						self.setFilter(CIFilter.pixellate())
+					},
+					.default(Text("Sepia Tone")) {
+						self.setFilter(CIFilter.sepiaTone())
+					},
+					.default(Text("Unsharp Mask")) {
+						self.setFilter(CIFilter.unsharpMask())
+					},
+					.default(Text("Vignette")) {
+						self.setFilter(CIFilter.vignette())
+					},
+					.default(Text("Cancel"))
+				])
 			}
 		}
 	}
@@ -92,14 +119,33 @@ struct ContentView: View {
 	}
 	
 	private func  applyProcessing() {
-		currentFilter.intensity = Float(filterIntensity)
+		let inputKeys = currentFilter.inputKeys
+		
+		
+		if inputKeys.contains(kCIInputIntensityKey) {
+			currentFilter.setValue(filterIntensity, forKey: kCIInputIntensityKey)
+		}
+		
+		if inputKeys.contains(kCIInputRadiusKey) {
+			currentFilter.setValue(filterIntensity * 200, forKey: kCIInputRadiusKey)
+		}
+		
+		if inputKeys.contains(kCIInputScaleKey) {
+			currentFilter.setValue(filterIntensity * 10, forKey: kCIInputScaleKey)
+		}
 		
 		guard let outputImage = currentFilter.outputImage else { return }
 		
 		if let cgimg = context.createCGImage(outputImage, from: outputImage.extent) {
 			let uiImage  = UIImage(cgImage: cgimg)
 			image  = Image(uiImage: uiImage)
+			processedImage = uiImage
 		}
+	}
+	
+	private func setFilter(_ filter: CIFilter) {
+		currentFilter = filter
+		loadImage()
 	}
 }
 
---
+++ Instafilter/Instafilter/ImageSaver.swift
@@ -0,0 +1,25 @@
+//
+//  ImageSaver.swift
+//  Instafilter
+//
+//  Created by Liem Vo on 11/28/19.
+//  Copyright © 2019 Liem Vo. All rights reserved.
+//
+
+import UIKit
+
+class ImageSaver: NSObject {
+	var successHandler: (() -> Void)?
+	var errorHandler: ((Error) -> Void)?
+	func writeToPhotoAlbum(image: UIImage) {
+		UIImageWriteToSavedPhotosAlbum(image, self, #selector(saveError), nil)
+	}
+	
+	@objc private func saveError(_ image: UIImage, didFinishSavingWithError error: Error?, contextInfo: UnsafeRawPointer) {
+		if let error = error {
+			errorHandler?(error)
+		} else {
+			successHandler?()
+		}
+	}
+}
