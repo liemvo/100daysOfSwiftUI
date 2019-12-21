bf48a56bbb0df7277e4c4e311ac1ed5ab303f026
Liem Vo <liemvt@gmail.com>
2019-12-21 20:34:55 +1300

    Day 88 of #100DaysOfSwiftUI

---
+++ Flashzilla/Flashzilla/models/Card.swift
@@ -0,0 +1,16 @@
+//
+//  Card.swift
+//  Flashzilla
+//
+//  Created by Liem Vo on 21/12/19.
+//  Copyright © 2019 Liem Vo. All rights reserved.
+//
+
+struct Card {
+	let prompt: String
+	let answer: String
+	
+	static var example: Card {
+		return Card(prompt: "Who played the 13th Doctor in Doctor Who?", answer: "Joddie Whittaker")
+	}
+}
